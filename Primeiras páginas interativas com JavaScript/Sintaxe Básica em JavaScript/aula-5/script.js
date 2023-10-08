56ecbb6e42c05eb16b8f91e9a1f0b0f2aedef7bf
Amanda Ribeiro <amanda_ribeiro98@yahoo.com.br>
2023-10-08 13:24:32 -0300

    final sintaxe basic in JavaScript

---
+++ Primeiras páginas interativas com JavaScript/Sintaxe Básica em JavaScript/aula-5/script.js
@@ -0,0 +1,9 @@
+function funcao () {
+    console.log('Mensagem de uma funcao');
+}
+funcao ();
+
+function mensagem (primeiro, segundo) {
+    console.log('mensagem de uma funcao', 'com parametros');
+}
+mensagem ();
---
+++ Primeiras páginas interativas com JavaScript/Sintaxe Básica em JavaScript/aula-6/tiposDeFuncao.js
@@ -0,0 +1,20 @@
+// // tipos de função
+
+// // declarativas
+// function funcao () {
+//     console.log('funcao declarativa');
+// }
+// funcao ();
+
+// //expressão de função
+// // com nomeação
+// var funcao = function () {
+//     console.log ('expressão de função com nomeação')
+// }
+// funcao ();
+
+// // arrow function =>
+// var funcao = () => {
+//     console.log ("função com arrow function");
+// }
+// funcao ();
